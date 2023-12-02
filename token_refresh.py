3f127b55f027b8a91ad0bd6fe0234d601b863f76
Gavie05 <83319954+Gavie05@users.noreply.github.com>
2023-12-02 05:48:10 -0500

    Update token_refresh.py

--- token_refresh.py
+++ token_refresh.py
@@ -1,8 +1,17 @@
 # Take refresh token and encoded auth string and return new tokens
-def get_new_token(refresh_token, auth_value):
+def get_new_token():
 
     import requests
+    import json
+
+    JSON_FILE_DIRECTORY = r'user_data.json'
     TOKEN_URL = 'https://accounts.spotify.com/api/token'
+    
+    open_file = open(JSON_FILE_DIRECTORY) #Open json into variable
+    json_data = json.load(open_file) #Load json into variable
+
+    refresh_token = json_data['REFRESH_TOKEN']
+    auth_value = json_data['AUTH_VALUE']
 
     # Refresh Token request data
     data = {
@@ -20,10 +29,7 @@ def get_new_token(refresh_token, auth_value):
     refresh_response = requests.post(TOKEN_URL, data=data,headers=headers)
 
     # Convert json data into strings
-    print(refresh_response.json())
     access_token = refresh_response.json()['access_token']
     expires_in = refresh_response.json()['expires_in']
 
     return access_token, expires_in
-
-
